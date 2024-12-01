6a82e32febaccb3ca9f8c8333480300d1d353d32
ShlomoNus <shlomonuswork@gmail.com>
2024-12-02 00:32:26 +0200

    refactor version up

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,50 @@
 export * from './utils/level';
 
-export * from './logger';
+import winston, { Logger } from 'winston';
+import { defaultColors, defaultLevels, LevelsType, LoggerColors } from './consts';
+import { defaultFormat, defaultTransports, FormatType, TransportsType } from './utils';
+
+type CreateLoggerInput = {
+    level?: string;
+    levels?: LevelsType;
+    format?: FormatType;
+    transports?: TransportsType;
+    colors: LoggerColors;
+};
+
+export class LoggerSingleton {
+    private static instance: LoggerSingleton;
+
+    private logger: Logger;
+
+    private constructor({ format, level, levels, transports, colors }: CreateLoggerInput) {
+        if (colors) winston.addColors(colors);
+
+        this.logger = winston.createLogger({
+            level,
+            levels,
+            format,
+            transports,
+        });
+    }
+
+    public static getInstance({
+        format = defaultFormat,
+        level = 'debug',
+        levels = defaultLevels,
+        transports = defaultTransports,
+        colors = defaultColors,
+    }: CreateLoggerInput): LoggerSingleton {
+        if (!LoggerSingleton.instance) {
+            LoggerSingleton.instance = new LoggerSingleton({
+                format,
+                level,
+                levels,
+                transports,
+                colors,
+            });
+        }
+
+        return LoggerSingleton.instance;
+    }
+}
